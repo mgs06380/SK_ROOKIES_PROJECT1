4e5e16ecf7f6634082306dd9ab665f609584b83d
조석훈 <.mgs06380@naver.com>
2024-07-25 12:59:17 +0900

    Revert "Revert "backend commit!""
    This reverts commit 5572573292abca58ecaaa3c1f05c6dca8abb326f.

---
+++ travelapp/src/main/java/configuration/DatabaseConfiguration.java
@@ -0,0 +1,35 @@
+package configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("mapper") // 패키지 경로를 정확히 지정합니다.
+public class DatabaseConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/travel_planner?useUnicode=true&characterEncoding=utf-8&serverTimezone=Asia/Seoul");
+        hikariConfig.setUsername("springboot");
+        hikariConfig.setPassword("p@ssw0rd");
+        return new HikariDataSource(hikariConfig);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setConfigLocation(new ClassPathResource("mybatis/mybatis-config.xml"));
+        return sessionFactory.getObject();
+    }
+}
